26d1aadd810e0e81ff8c32ccf2d0c22227915ffc
RomaPux228 <50229125+RomaPux228@users.noreply.github.com>
2019-05-03 13:04:32 +0300

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Panel, ListItem, Button, Group, Div, Avatar, PanelHeader } from '@vkontakte/vkui';
+
+const Home = ({ id, go, fetchedUser }) => (
+	<Panel id={id}>
+		<PanelHeader>Главное Меню</PanelHeader>
+		{fetchedUser &&
+		<Group title="Игровой профиль">
+			<ListItem
+				before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
+				description={fetchedUser.city && fetchedUser.city.title ? fetchedUser.city.title : ''}
+			>
+				{`${fetchedUser.first_name} ${fetchedUser.last_name}`}
+			</ListItem>
+		</Group>}
+
+		<Group title="Команды">
+			<Div>
+				<Button size="xl" level="1" onClick={go} data-to="persik">
+					клик					
+				</Button>  
+			</Div>
+		</Group>
+	</Panel>
+);
+
+Home.propTypes = {
+	id: PropTypes.string.isRequired,
+	go: PropTypes.func.isRequired,
+	fetchedUser: PropTypes.shape({
+		photo_200: PropTypes.string,
+		first_name: PropTypes.string,
+		last_name: PropTypes.string,
+		city: PropTypes.shape({
+			title: PropTypes.string,
+		}),
+	}),
+};
+
+export default Home;
